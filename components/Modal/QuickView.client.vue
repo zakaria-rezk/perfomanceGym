1b24881a5fe19d657edd4a3578732f2ed7acb8d9
zekor <zakariarezk0@gmail.com>
2024-08-17 20:51:37 +0300

    product desing

--- components/Modal/QuickView.client.vue
+++ components/Modal/QuickView.client.vue
@@ -4,38 +4,20 @@
       <v-container class="content rounded-xl position-relative w-75">
         <v-row>
           <v-col cols="6">
-            <v-btn variant="plain" size="xx-l"  ref="emitBtn"
-              ><v-icon >mdi-close</v-icon></v-btn
-            >
-            <div class="text-right">
+            <img :src="Props.imgSrc" class="img" />
+          </v-col>
+          <v-col cols="6">
+            <div  class="text-right">
+            <v-btn variant="plain" size="xx-l" ref="emitBtn" 
+              ><v-icon>mdi-close</v-icon></v-btn
+            ></div>
+            <div >
               <p class="my-4 text-h5">{{ Props.ProName }}</p>
               <p class="text-red text-h4">{{ Props.ProPrice }} USD</p>
-              <div class="d-flex justify-end counter-div">
-                <v-btn class="bg-red" size="xx-large">Add to cart</v-btn>
-                <div class="d-flex text counter mx-5">
-                  <button @click="counter++" class="text-center btn btn-r">
-                    <v-icon size="15">mdi-plus</v-icon></button
-                  ><input
-                    class="input text-center"
-                    type="number"
-                    :value="counter"
-                  />
-
-                  <button
-                    @click="counter--"
-                    class="text-center btn btn-l"
-                    :disabled="counter === 1"
-                  >
-                    <v-icon size="15">mdi-minus</v-icon>
-                  </button>
-                </div>
-              </div>
+              <Cart />
               <v-divider class="my-5"></v-divider>
               <p class="text-left">Category: {{ Props.ProCategory }}</p>
             </div>
-          </v-col>
-          <v-col cols="6">
-            <img :src="Props.imgSrc" class="img" />
           </v-col>
         </v-row>
       </v-container>
@@ -50,19 +32,20 @@ interface props {
   ProPrice: number;
   ProCategory: string;
 }
-const counter = ref<number>(1);
+
 const emit = defineEmits<{
   (event: "closeModal"): void;
 }>();
 
 const Props = defineProps<props>();
 const handleClickOutSide = (event: Event) => {
-  if(!event.target) return
-  if(event.target === container.value || event.target.classList.contains('mdi-close')){
-  emit('closeModal')
+  if (!event.target) return;
+  if (
+    event.target === container.value ||
+    event.target.classList.contains("mdi-close")
+  ) {
+    emit("closeModal");
   }
-
- 
 };
 onBeforeMount(() => {
   document.addEventListener("click", handleClickOutSide);
@@ -75,35 +58,6 @@ onBeforeUnmount(() => {
 });
 </script>
 <style scoped>
-.counter-div {
-  height: 50px;
-}
-.btn {
-  width: 30px;
-}
-.btn-r:hover {
-  background-color: brown;
-  border-radius: 25px 0 0 25px;
-}
-.btn-l:hover {
-  background-color: brown;
-  border-radius: 0 25px 25px 0;
-}
-.counter {
-  border: 1px solid gray;
-  border-radius: 25px;
-  width: 80px;
-  height: 42px;
-}
-input {
-  width: 25px;
-  border: 1px solid gray;
-}
-input[type="number"]::-webkit-inner-spin-button,
-input[type="number"]::-webkit-outer-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
 .container {
   display: flex;
   justify-content: center;
@@ -114,7 +68,6 @@ input[type="number"]::-webkit-outer-spin-button {
   z-index: 10000;
   background-color: rgba(42, 42, 44, 0.8);
   overflow-y: scroll;
-  
 }
 .content {
   bottom: 10px;
@@ -123,7 +76,7 @@ input[type="number"]::-webkit-outer-spin-button {
   height: 100%;
   transition: all 0.3s;
   transform: translateX(50px);
-  
+
   animation: showmodal 0.3s ease-in-out forwards;
 }
 .img {
@@ -134,9 +87,8 @@ input[type="number"]::-webkit-outer-spin-button {
 @keyframes showmodal {
   form {
     opacity: 0;
-    
   }
-  to{
+  to {
     opacity: 1;
     transform: translateX(0);
   }
--- pages/Product/[product].vue
+++ pages/Product/[product].vue
@@ -1,10 +1,70 @@
 <template>
   <div>
-    <h2 style="margin-top: 115px">prdsfaaaaodcut.vue</h2>
+    <v-container style="margin-top: 115px"
+      ><v-row>
+        <v-col cols="6" class="d-md-flex justify-end">
+          <div class="d-flex flex-column">
+            <img
+              src="/assets/imges/products/Picsart_24-01-26_07-57-49-568-150x225.png"
+              alt=""
+              class="my-2"
+            />
+            <img
+              src="/assets/imges/products/Picsart_24-01-26_07-57-49-568-150x225.png"
+              alt=""
+            />
+          </div>
+          <div class="position-relative d-flex align-center m-">
+            <v-btn variant="text" class="mr-n16"
+              ><v-icon>mdi-arrow-left</v-icon></v-btn
+            >
+            <img
+              src="/assets/imges/products/Picsart_24-01-26_07-57-49-568-150x225.png"
+              alt=""
+              class="my-2 mx-5 mainImg"
+            />
+            <v-btn variant="text" class="ml-n16"
+              ><v-icon>mdi-arrow-right</v-icon></v-btn
+            >
+          </div> </v-col
+        ><v-col cols="6" class="">
+          <div class="d-flex justify-space-between">
+            <div class="text-black mx-15">
+              <v-breadcrumbs :items="items"></v-breadcrumbs>
+              <h1>sdfdsfdsaf</h1>
+              <h3>1500 sud</h3>
+              <Cart />
+            </div>
+            <div class="mt-4">
+              <v-btn variant="text" size="xx-small" class="mx-1"
+                ><v-icon size="small">mdi-arrow-left</v-icon></v-btn
+              >
+              <v-btn variant="text" class="mx-1" size="xx-small"
+                ><v-icon>mdi-view-grid-outline</v-icon></v-btn
+              >
+              <v-btn size="xx-small" variant="text" class="mx-1"
+                ><v-icon size="small">mdi-arrow-right</v-icon></v-btn
+              >
+            </div>
+          </div>
+        </v-col>
+        </v-row
+    ></v-container>
   </div>
 </template>
-<script>
+<script setup lang="ts">
 definePageMeta({
   layout: "custom",
 });
+const items = ref<string[]>(["home", "wheyPortain", "mokka"]);
+
+onMounted(() => {
+  const router = useRouter();
+  console.log(router);
+});
 </script>
+<style scoped>
+.mainImg {
+  height: 100vh;
+}
+</style>
