ee1b500f9b7feda33b3f933f7bd9ec14f0d6bd81
zekor <zakariarezk0@gmail.com>
2024-09-21 15:28:58 +0300

    Afas

--- components/Product.vue
+++ components/Product.vue
@@ -152,9 +152,9 @@
 
 <script setup lang="ts">
 const url =
-  "/static/images/products/WheyPortain/IMG_20230306_021505-860x982.png";
+  "https://master--leafy-alfajores-fc6788.netlify.app/_nuxt/static/images/products/WheyPortain/IMG_20230306_021505-860x982.png";
 const url2 =
-  "require('https://master--leafy-alfajores-fc6788.netlify.app/static/images/products/WheyPortain/IMG_20230306_021505-860x982.png')";
+  "'https://master--leafy-alfajores-fc6788.netlify.app/static/images/products/WheyPortain/IMG_20230306_021505-860x982.png'";
 import { useProductStore } from "~/sotres/ProductSotre";
 const productStore = useProductStore();
 const router = useRouter();
