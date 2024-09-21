830d9356797837f35d22716e4d8fc5908d4dd803
zekor <zakariarezk0@gmail.com>
2024-09-21 03:31:34 +0300

    sadfasF

--- sotres/Product.ts
+++ sotres/Product.ts
@@ -10,8 +10,8 @@ export const useUserStore = defineStore("user", {
           name: "WHEY PROTIEN OREO 1.5k",
           price: 100,
           imgs: [
-            "https://master--precious-medovik-187566.netlify.app/static/imges/products/WheyPortain/Picsart_24-01-26_06-54-21-965.png",
-            "https://master--precious-medovik-187566.netlify.app/static/imges/products/WheyPortain/IMG_20240221_020330-768x947.png",
+            "../static/imges/products/WheyPortain/Picsart_24-01-26_06-54-21-965.png",
+            "../static/imges/products/WheyPortain/IMG_20240221_020330-768x947.png",
           ],
           category: "WheyProtain",
           details:
@@ -42,9 +42,9 @@ export const useUserStore = defineStore("user", {
           name: "CASEIN PROTEIN STRWPERRY",
           price: 1250,
           imgs: [
-            "https://master--precious-medovik-187566.netlify.app/static/imges/products/WheyPortain/IMG_20240126_055846.png",
-            "https://master--precious-medovik-187566.netlify.app/static/imges/products/WheyPortain/IMG_20240208_160203.png",
-            "https://master--precious-medovik-187566.netlify.app/static/imges/products/WheyPortain/1-e1676102590559-1192x1536.png",
+            "../static/imges/products/WheyPortain/IMG_20240126_055846.png",
+            "../static/imges/products/WheyPortain/IMG_20240208_160203.png",
+            "../static/imges/products/WheyPortain/1-e1676102590559-1192x1536.png",
           ],
           category: "WheyProtain",
           details:
@@ -79,9 +79,9 @@ export const useUserStore = defineStore("user", {
           name: "WHEY OREO 1kg",
           price: 1500,
           imgs: [
-            "https://master--precious-medovik-187566.netlify.app/static/imges/products/WheyPortain/IMG_20240126_055056-700x1013.png",
-            "https://master--precious-medovik-187566.netlify.app/static/imges/products/WheyPortain/IMG_20230306_021505-860x982.png",
-            "https://master--precious-medovik-187566.netlify.app/static/imges/products/WheyPortain/1-e1676102590559-1192x1536.png",
+            "../static/imges/products/WheyPortain/IMG_20240126_055056-700x1013.png",
+            "../static/imges/products/WheyPortain/IMG_20230306_021505-860x982.png",
+            "../static/imges/products/WheyPortain/1-e1676102590559-1192x1536.png",
           ],
           category: "WheyProtain",
           details:
@@ -116,8 +116,8 @@ export const useUserStore = defineStore("user", {
           name: "CASEIN PROTEIN CHOCOLATE",
           price: 1800,
           imgs: [
-            "https://master--precious-medovik-187566.netlify.app/_nuxt/static/imges/products/WheyPortain/IMG_20240126_061031.png",
-            "/static/imges/products/WheyPortain/IMG_20240208_160218-768x1032.png",
+            "../_nuxt/static/imges/products/WheyPortain/IMG_20240126_061031.png",
+            "../static/imges/products/WheyPortain/IMG_20240208_160218-768x1032.png",
             "/static/imges/products/WheyPortain/IMG_20240208_160218-768x1032.png",
             "/static/imges/products/WheyPortain/IMG_20240208_160218-768x1032.png",
           ],
